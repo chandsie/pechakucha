80b8daa37e9bd5afcb2c56cc354e7f82cc20078d
Shreyas Chand <shreyas.chand@gmail.com>
2015-08-10 02:59:28 -0700

    Reimplement basics

--- app/src/main/java/com/shreyaschand/pechakucha/MainActivity.java
+++ app/src/main/java/com/shreyaschand/pechakucha/MainActivity.java
@@ -4,58 +4,158 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText timerDisplay;
-    CountDownTimer countDownTimer;
-    private Button stopButton;
-    private Button startButton;
+    @Bind(R.id.period_time_remaining) EditText periodTimeDisplay;
+    @Bind(R.id.periods_remaining)     EditText periodsDisplay;
+
+    @Bind(R.id.start_button)  Button startButton;
+    @Bind(R.id.pause_button)  Button pauseButton;
+    @Bind(R.id.resume_button) Button resumeButton;
+
+    private int totalPeriods;
+    private int periodLength;
+    private int timeLeftOnPause;
+    private PechaKuchaTimer countDownTimer;
+    private TextWatcher textWatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        timerDisplay = (EditText) findViewById(R.id.time_remaining);
-        startButton = (Button) findViewById(R.id.start_button);
-        stopButton = (Button) findViewById(R.id.stop_button);
+        ButterKnife.bind(this);
+        textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // no-op
+            }
 
-        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-               startCountdown(20);
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setupInitialInterface();
             }
-        });
-        stopButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View v) {
-                if (countDownTimer != null) {
-                    countDownTimer.cancel();
-                    countDownTimer = null;
-                }
+            public void afterTextChanged(Editable s) {
+                // no-op
             }
-        });
+        };
     }
 
-    public void startCountdown(final int seconds) {
-        if (countDownTimer != null) {
-            countDownTimer.cancel();
+    @OnClick(R.id.start_button)
+    public void onStartStopButtonClick(Button b) {
+        // if we're just starting the timer, read from the text fields
+        if (TextUtils.equals(b.getText(), getString(R.string.start))) {
+            totalPeriods = getNumber(periodsDisplay);
+            periodLength = getNumber(periodTimeDisplay);
+        } // else use the previously read values to restart the timer
+
+        countDownTimer = new PechaKuchaTimer(totalPeriods, periodLength);
+        countDownTimer.start();
+
+        setupRunningTimerInterface();
+    }
+
+    @OnClick(R.id.pause_button)
+    public void onPauseButtonClick() {
+        countDownTimer.cancel();
+        timeLeftOnPause = countDownTimer.getSecondsLeft();
+
+        setupStoppedTimerInterface();
+        resumeButton.setVisibility(View.VISIBLE);
+    }
+
+    @OnClick(R.id.resume_button)
+    public void onResumeButtonClick() {
+        countDownTimer = new PechaKuchaTimer(timeLeftOnPause + 1); // add a buffer second 
+        countDownTimer.start();
+        setupRunningTimerInterface();
+    }
+
+    private void setupInitialInterface() {
+        periodsDisplay.setTextColor(Color.WHITE);
+        periodTimeDisplay.setTextColor(Color.WHITE);
+
+        startButton.setText(R.string.start);
+        startButton.setVisibility(View.VISIBLE);
+        resumeButton.setVisibility(View.GONE);
+    }
+
+    private void setupRunningTimerInterface() {
+        periodsDisplay.setEnabled(false);
+        periodsDisplay.removeTextChangedListener(textWatcher);
+        periodTimeDisplay.setEnabled(false);
+        periodTimeDisplay.removeTextChangedListener(textWatcher);
+
+        startButton.setVisibility(View.GONE);
+        resumeButton.setVisibility(View.GONE);
+        pauseButton.setVisibility(View.VISIBLE);
+    }
+
+    private void setupStoppedTimerInterface() {
+        periodsDisplay.setEnabled(true);
+        periodsDisplay.addTextChangedListener(textWatcher);
+        periodTimeDisplay.setEnabled(true);
+        periodTimeDisplay.addTextChangedListener(textWatcher);
+
+        startButton.setText(R.string.restart);
+        startButton.setVisibility(View.VISIBLE);
+        pauseButton.setVisibility(View.GONE);
+    }
+
+
+    private int getNumber(EditText editText) {
+        String text = editText.getText().toString();
+        if (TextUtils.isEmpty(text)) {
+            text = editText.getHint().toString();
+        }
+        return Integer.parseInt(text);
+    }
+
+
+    private class PechaKuchaTimer extends CountDownTimer {
+
+        int secondsLeft;
+
+        public PechaKuchaTimer(int seconds) {
+            super(seconds * 1000, 1000);
         }
 
-        timerDisplay.setTextColor(Color.BLACK);
-        countDownTimer = new CountDownTimer(seconds * 1000 * 6, 1000) {
+        public PechaKuchaTimer(int periodLength, int numPeriods) {
+            super(periodLength * numPeriods * 1000, 1000);
+        }
 
-            public void onTick(long millisUntilFinished) {
-                timerDisplay.setText(Integer.toString((((int) millisUntilFinished / 1000) % seconds) + 1));
-            }
+        @Override
+        public void onTick(long millisUntilFinished) {
+            secondsLeft = (int) (millisUntilFinished / 1000);
+            periodTimeDisplay.setText(Integer.toString(secondsLeft % periodLength));
+            periodsDisplay.setText(Integer.toString((secondsLeft / periodLength) + 1));
+        }
 
-            public void onFinish() {
-                timerDisplay.setText(Integer.toString(0));
-                timerDisplay.setTextColor(Color.RED);
-            }
-        }.start();
+        @Override
+        public void onFinish() {
+            periodTimeDisplay.setText("0");
+            periodTimeDisplay.setTextColor(Color.RED);
+
+            periodsDisplay.setText("0");
+            periodsDisplay.setTextColor(Color.RED);
+
+            setupStoppedTimerInterface();
+        }
+
+        public int getSecondsLeft() {
+            return secondsLeft;
+        }
     }
 }
